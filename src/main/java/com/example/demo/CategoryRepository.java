b4bd3cb8ff1a1111265ab1662f8bacea3fd18f37
romielisse <romielisse@gmail.com>
2019-03-18 09:23:26 -0400

    initial commit

--- src/main/java/com/example/demo/CategoryRepository.java
+++ src/main/java/com/example/demo/CategoryRepository.java
@@ -3,4 +3,5 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+	Iterable<Category> findAllByCategory(String category);
 }
